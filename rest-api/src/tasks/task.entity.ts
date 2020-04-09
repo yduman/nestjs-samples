8da9f497278c7a1555f3a28d7059d2d213dbb77e
Yadullah Duman <yadullah.duman@maibornwolff.de>
2020-04-09 20:08:47 +0200

    define task entity

---
+++ rest-api/src/tasks/task.entity.ts
@@ -0,0 +1,17 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { TaskStatus } from './task.model';
+
+@Entity()
+export class TaskEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
+}
