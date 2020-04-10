2c490aea6b309b48ff848069a5dc8630a65eb3ec
Yadullah Duman <yadullah.duman@maibornwolff.de>
2020-04-10 18:10:03 +0200

    add auth module and user entity

--- rest-api/src/app.module.ts
+++ rest-api/src/app.module.ts
@@ -3,8 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TasksModule } from './tasks/tasks.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule],
 })
 export class AppModule {}
