57b77c4be09c4b0b6ff33ef574d0a1ef898d428e
Yadullah Duman <yadullah.duman@maibornwolff.de>
2020-04-11 17:20:54 +0200

    add unique username constraints and hash passwords on sign up

--- rest-api/src/auth/user.repository.ts
+++ rest-api/src/auth/user.repository.ts
@@ -1,4 +1,9 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Repository, EntityRepository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -7,11 +12,25 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-
     const user = new User();
-    user.username = username;
-    user.password = password;
+    const salt = await bcrypt.genSalt();
 
-    await user.save();
+    user.username = username;
+    user.salt = salt;
+    user.password = await this.hashPassword(password, salt);
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
