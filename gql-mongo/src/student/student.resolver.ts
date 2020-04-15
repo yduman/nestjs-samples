b7c081a054eeac5ce8589ebac5543fb2b5d9df14
Yadullah Duman <yadullah.duman@maibornwolff.de>
2020-04-15 19:03:02 +0200

    add resolver for fetching all students

--- gql-mongo/src/student/student.resolver.ts
+++ gql-mongo/src/student/student.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 
 import { StudentType } from './student.type';
 import { CreateStudentInput } from './create-student.input';
@@ -7,6 +7,11 @@ import { StudentService } from './student.service';
 @Resolver(of => StudentType)
 export class StudentResolver {
   constructor(private studentService: StudentService) {}
+
+  @Query(returns => [StudentType])
+  async students() {
+    return this.studentService.getStudents();
+  }
 
   @Mutation(returns => StudentType)
   async createStudent(
